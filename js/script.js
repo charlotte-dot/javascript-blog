808a6eaedc0a0377e671542c7825b3965a501819
Karolina Smolinska <konto.dev2@gmail.com>
2022-01-30 21:33:35 +0100

    clickedElement class add

--- js/script.js
+++ js/script.js
@@ -13,31 +13,35 @@
         link.addEventListener('click', titleClickHandler);
     }
 
-    const titleClickHandler = function (event) {
+    const titleClickHandler = function(event){
+        const clickedElement = this;
         console.log('Link was clicked!');
-
-        /* remove class 'active' from all article links  */
+      
+        /* [DONE] remove class 'active' from all article links  */
+      
         const activeLinks = document.querySelectorAll('.titles a.active');
-
-        for (let activeLink of activeLinks) {
-            activeLink.classList.remove('active');
+      
+        for(let activeLink of activeLinks){
+          activeLink.classList.remove('active');
         }
-
-        /* add class 'active' to the clicked link */
-
-        /* remove class 'active' from all articles */
-
+      
+        /* [IN PROGRESS] add class 'active' to the clicked link */
+        clickedElement.classList.add('active')
+        console.log('clickedElement:', clickedElement);
+      
+        /* [DONE] remove class 'active' from all articles */
+      
         /* get 'href' attribute from the clicked link */
-
+      
         /* find the correct article using the selector (value of 'href' attribute) */
-
+      
         /* add class 'active' to the correct article */
-    }
-
-    const links = document.querySelectorAll('.titles a');
-
-    for (let link of links) {
+      }
+      
+      const links = document.querySelectorAll('.titles a');
+      
+      for(let link of links){
         link.addEventListener('click', titleClickHandler);
-    }
+      }
 
 }
